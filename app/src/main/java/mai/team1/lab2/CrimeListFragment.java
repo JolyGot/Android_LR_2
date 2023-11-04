dbeca3fae1a0d43067c53b1216d0bf95c0490faf
JolyGot <kuznetsov_akn@mail.ru>
2023-11-04 21:26:49 +0300

    Глава 9

--- app/src/main/java/mai/team1/lab2/CrimeListFragment.java
+++ app/src/main/java/mai/team1/lab2/CrimeListFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,6 +18,7 @@ import java.util.List;
 public class CrimeListFragment  extends Fragment {
     private RecyclerView mCrimeRecyclerView;
     private CrimeAdapter mAdapter;
+    private ImageView mSolvedImageView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -46,12 +48,14 @@ public class CrimeListFragment  extends Fragment {
                 mTitleTextView = (TextView) itemView.findViewById(R.id.crime_title);
                 mDateTextView = (TextView) itemView.findViewById(R.id.crime_date);
                 itemView.setOnClickListener(this);
+                mSolvedImageView = (ImageView) itemView.findViewById(R.id.crime_solved);
 
             }
                 public void bind (Crime crime){
                 mCrime = crime;
                 mTitleTextView.setText(mCrime.getTitle());
                 mDateTextView.setText(mCrime.getDate().toString());
+                mSolvedImageView.setVisibility(crime.isSolved() ? View.VISIBLE : View.GONE);
             }
                 @Override
                 public void onClick (View view){
