f1895e687ca504f883724f0c2cccf7cb1179dbc9
JolyGot <kuznetsov_akn@mail.ru>
2023-11-05 11:01:34 +0300

    Глава 10

--- app/src/main/java/mai/team1/lab2/CrimeListFragment.java
+++ app/src/main/java/mai/team1/lab2/CrimeListFragment.java
@@ -1,6 +1,7 @@
 package mai.team1.lab2;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -32,11 +33,21 @@ public class CrimeListFragment  extends Fragment {
         return view;
 
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        updateUI();
+    }
     private void updateUI() {
         CrimeLab crimeLab = CrimeLab.get(getActivity());
         List<Crime> crimes = crimeLab.getCrimes();
-        mAdapter = new CrimeAdapter(crimes);
-        mCrimeRecyclerView.setAdapter(mAdapter);
+        if (mAdapter == null) {
+            mAdapter = new CrimeAdapter(crimes);
+            mCrimeRecyclerView.setAdapter(mAdapter);
+        } else {
+            mAdapter.notifyDataSetChanged();
+        }
     }
         private class CrimeHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
                 private TextView mTitleTextView;
@@ -59,9 +70,8 @@ public class CrimeListFragment  extends Fragment {
             }
                 @Override
                 public void onClick (View view){
-                Toast.makeText(getActivity(),
-                                mCrime.getTitle() + " clicked!", Toast.LENGTH_SHORT)
-                        .show();
+                    Intent intent = CrimeActivity.newIntent(getActivity(), mCrime.getId());
+                    startActivity(intent);
             }
     }
 
